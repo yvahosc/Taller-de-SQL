3d5c4a66069ca3dd6946ed0a182ef809ed383052
yvahosc <yvahosc@unal.edu.co>
2022-03-12 15:14:53 -0500

    Actualización script 2
    Creación de las consultas por cliente para obtener los productos comprados por este en las diferentes compras realizadas.

--- script2.sql
+++ script2.sql
@@ -1,5 +1,24 @@
--- 1. Consulta productos vendidos por numero de documento
+-- 1. Consulta productos vendidos digitando tipo de documento y número de documento.
+-- Consulta productos por cliente 1
+SELECT prod_name FROM product WHERE prod_id = 
+    ANY (SELECT prod_id FROM product_in_sale 
+    WHERE sale_id = ANY (SELECT sale_id FROM sale 
+    WHERE cust_id = (SELECT cust_id FROM customer 
+    WHERE cust_type_id = 'CC' AND cust_id_number = '0000000001')));
 
+-- Consulta productos por cliente 2
+SELECT prod_name FROM product WHERE prod_id = 
+    ANY (SELECT prod_id FROM product_in_sale 
+    WHERE sale_id = ANY (SELECT sale_id FROM sale 
+    WHERE cust_id = (SELECT cust_id FROM customer 
+    WHERE cust_type_id = 'CC' AND cust_id_number = '0000000002')));
+
+-- Consulta productos por cliente 3
+SELECT prod_name FROM product WHERE prod_id = 
+    ANY (SELECT prod_id FROM product_in_sale 
+    WHERE sale_id = ANY (SELECT sale_id FROM sale 
+    WHERE cust_id = (SELECT cust_id FROM customer 
+    WHERE cust_type_id = 'CE' AND cust_id_number = '0000000003')));
 
 -- 2. Consulta de productos por nombre y mostrar proveedor 
 

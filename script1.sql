4318dce923d7e1cd76ddab8ebb2a61ef58aef597
yvahosc <yvahosc@unal.edu.co>
2022-03-12 10:22:51 -0500

    Actualización script 1
    Modificación de index en la creación de las tablas y creación de información para cada una de las tablas

--- script1.sql
+++ script1.sql
@@ -8,7 +8,8 @@ USE store;
 CREATE TABLE IF NOT EXISTS supplier (
     sup_id INT NOT NULL AUTO_INCREMENT,
     sup_name VARCHAR(80) NOT NULL,
-    PRIMARY KEY (sup_id))
+    PRIMARY KEY (sup_id),
+    UNIQUE INDEX sup_name_UNIQUE (sup_name ASC) VISIBLE)
 ENGINE = InnoDB;
 
 -- Creación tabla product
@@ -17,7 +18,7 @@ CREATE TABLE IF NOT EXISTS product (
     sup_id INT NOT NULL,
     prod_name VARCHAR(80) NOT NULL,
     PRIMARY KEY (prod_id),
-    UNIQUE INDEX fk_product_supplier_idx (sup_id ASC) VISIBLE,
+    UNIQUE INDEX fk_product_supplier_idx (prod_name ASC) VISIBLE,
     CONSTRAINT fk_product_supplier
         FOREIGN KEY (sup_id)
         REFERENCES supplier (sup_id)
@@ -40,7 +41,7 @@ CREATE TABLE IF NOT EXISTS sale (
     cust_id INT NOT NULL,
     sale_elimination_status BOOLEAN NULL DEFAULT 0,
     PRIMARY KEY (sale_id),
-    UNIQUE INDEX fk_sale_customer1_idx (cust_id ASC) VISIBLE,
+    UNIQUE INDEX fk_sale_customer1_idx (sale_id ASC) VISIBLE,
     CONSTRAINT fk_sale_customer1
         FOREIGN KEY (cust_id)
         REFERENCES customer (cust_id)
@@ -70,6 +71,56 @@ ENGINE = InnoDB;
 
 
 -- 2. Creación de información para las tablas
+
+-- Inserción en tabla supplier
+INSERT INTO supplier (sup_name) VALUES ('LG');
+INSERT INTO supplier (sup_name) VALUES ('HP');
+INSERT INTO supplier (sup_name) VALUES ('SONY');
+INSERT INTO supplier (sup_name) VALUES ('LENOVO');
+INSERT INTO supplier (sup_name) VALUES ('PANASONIC');
+
+-- Inserción en tabla product
+INSERT INTO product (sup_id, prod_name) VALUES (1, 'Aire acondicionado');
+INSERT INTO product (sup_id, prod_name) VALUES (1, 'Lavadora');
+INSERT INTO product (sup_id, prod_name) VALUES (2, 'Computador');
+INSERT INTO product (sup_id, prod_name) VALUES (3, 'Equipo de sonido');
+INSERT INTO product (sup_id, prod_name) VALUES (3, 'Play Station');
+INSERT INTO product (sup_id, prod_name) VALUES (4, 'Celular inteligente');
+INSERT INTO product (sup_id, prod_name) VALUES (5, 'Televisor');
+
+-- Inserción en tabla customer
+INSERT INTO customer (cust_type_id, cust_id_number) VALUES ('CC', '0000000001');
+INSERT INTO customer (cust_type_id, cust_id_number) VALUES ('CC', '0000000002');
+INSERT INTO customer (cust_type_id, cust_id_number) VALUES ('CE', '0000000003');
+
+-- Inserción en tabla sale
+INSERT INTO sale (cust_id) VALUES (1);
+INSERT INTO sale (cust_id) VALUES (1);
+INSERT INTO sale (cust_id) VALUES (3);
+INSERT INTO sale (cust_id) VALUES (2);
+INSERT INTO sale (cust_id) VALUES (3);
+
+
+-- Inserción en tabla product_in_sale
+-- Venta 1
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (1, 5, 1);
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (1, 3, 2);
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (1, 7, 5);
+
+-- Venta 2
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (2, 1, 8);
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (2, 2, 4);
+
+-- Venta 3
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (3, 4, 3);
+
+-- Venta 4
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (4, 6, 6);
+
+-- Venta 5
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (5, 1, 1);
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (5, 2, 1);
+INSERT INTO product_in_sale (sale_id, prod_id, product_quantity_per_sale) VALUES (5, 4, 1);
 
 
 -- 3. Borrados lógicos y físicos de ventas realizadas
